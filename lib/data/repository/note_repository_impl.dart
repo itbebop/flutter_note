785887932d3aef9285e14ba6a0adcac541e9facf
itbebop <it.bebop@gmail.com>
2023-05-13 21:37:15 +0900

    구조 잡기

---
+++ lib/data/repository/note_repository_impl.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_note/data/data_source/note_db.dart';
+import 'package:flutter_note/domain/model/note.dart';
+
+import '../../domain/repository/note_repository.dart';
+
+class NoteRepositoryImpl implements NoteRepository {
+  final NoteDb db;
+
+  NoteRepositoryImpl(this.db);
+
+  @override
+  Future<void> deleteNote(Note note) {
+    // TODO: implement deleteNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Note> getNoteById(int id) {
+    // TODO: implement getNoteById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Note>> getNotes() {
+    // TODO: implement getNotes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> insertNote(Note note) {
+    // TODO: implement insertNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateNote(Note note) {
+    // TODO: implement updateNote
+    throw UnimplementedError();
+  }
+}
