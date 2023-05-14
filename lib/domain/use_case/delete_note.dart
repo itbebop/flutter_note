784f457f4c72fc14ec82b6385a6daa1f8e26426f
itbebop <it.bebop@gmail.com>
2023-05-14 11:32:12 +0900

    useCase 작성

---
+++ lib/domain/use_case/delete_note.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_note/domain/repository/note_repository.dart';
+
+import '../model/note.dart';
+
+class DeleteNote {
+
+  final NoteRepository repository;
+
+  DeleteNote(this.repository);
+
+  Future<void> call(Note note) async {
+    await repository.deleteNote(note);
+  }
+}
